75648db6c5a99fa8c1387194bb0dc7a13e82eef0
c-abeka <havelstech@gmail.com>
2022-12-01 02:24:52 +0300

    formatted 100 to use one printf

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+i = 0
 for  x in reversed( range(97, 123)):
-    if x % 2 == 0:
-        print(chr(x), end='')
-    elif x % 2 != 0:
-        print(chr(x - (ord('a') - ord('A'))), end='')
-        
-
+    print(chr(x - i), end='')
+    if i == 0:
+        i = 32
+    else:
+        i = 0
