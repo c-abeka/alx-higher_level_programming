2bf27afb7ddbbb927745b251e4b43213caf1f9b0
c-abeka <havelstech@gmail.com>
2022-12-01 23:28:13 +0300

    task 2 add plural for more than one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     if argc == 0:
         print("{:d} arguments.".format(argc))
     if argc >= 1:
-        print("{:d} arguments:".format(argc))
+        s = ''
+        if argc > 1:
+            s = 's'
+        print("{:d} argument{}:".format(argc, s))
         for a in argz:
             print("{:d}: {}".format(i + 1, argz[i]))
             i += 1
