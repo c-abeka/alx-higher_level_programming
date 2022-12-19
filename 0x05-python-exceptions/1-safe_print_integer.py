7ebdc35f2d5ae8337da75dc3b08ef9c3c7a4e452
c-abeka <havelstech@gmail.com>
2022-12-20 02:08:41 +0300

    task 2 print integers only

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    x = 1
     try:
         val = int(value)
         print("{:d}".format(val))
         return True
     except ValueError:
-        x = 0
-    finally:
-        return bool(x)
+        return False
