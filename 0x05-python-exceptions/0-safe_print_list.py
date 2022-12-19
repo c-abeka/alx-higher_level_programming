d2ecc20e98753e95392a797cbf6b6cf0836d7106
c-abeka <havelstech@gmail.com>
2022-12-20 01:55:11 +0300

    task 0 try: / except:

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        for el in my_list:
+            if i < x:
+                print("{}".format(el), end='')
+                i += 1
+        print()
+        return i
+    except Exception:
+        raise 
