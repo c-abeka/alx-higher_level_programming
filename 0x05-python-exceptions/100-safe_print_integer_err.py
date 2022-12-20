5e0ac8ce4dd3a0229e0e614cb6b3ab84597bbe16
c-abeka <havelstech@gmail.com>
2022-12-20 04:34:32 +0300

    task 100 write error to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as ve:
+    except Exception as ve:
         import sys
         sys.stderr.write("Exception: {}\n".format(ve))
         return False
