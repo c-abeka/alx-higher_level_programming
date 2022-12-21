58f9c74939d68daf7f02659bd7c504e61a88d445
c-abeka <havelstech@gmail.com>
2022-12-21 03:42:55 +0300

    print format

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,12 +43,20 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(self.__size):
-            if self.__position[0] == 0:
-                print('#' * self.__size)
+            return None
+
+        if self.__position[1] > 0:
+            for i in range(self.__position[1]):
+                print('')
+
+        for j in range(1, self.area() + 1):
+            if j % self.__size == 1:
+                print('{:>{w}}'.format('#', w=self.__position[0] + 1), end='')
             else:
-                print(" " * self.__position[0], end='')
-                print('#' * self.__size)
+                print('#', end='')
+
+            if j % self.__size == 0 and j > 0:
+                print()
 
     @property
     def position(self):
