1ef4b261fd92a9f5013f6f2c3f6ed449f476a7f1
c-abeka <havelstech@gmail.com>
2022-12-21 03:31:38 +0300

    test check for int

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,5 +57,6 @@ class Square:
     @position.setter
     def position(self, value):
         if type(value) is not tuple or len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
+            if type(value[0]) or type(value[1]) is not int:
+                raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
