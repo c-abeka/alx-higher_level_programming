436d7e4310fdb16a33b80dbd746966fcf1dc1bc9
c-abeka <havelstech@gmail.com>
2022-12-21 05:23:40 +0300

    task 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -74,10 +74,8 @@ class Square:
 
     def __str__(self):
         if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
+            [print('') for i in range(self.position[1])]
+        for i in range(self.__size):
+            print(" " * self.__position[0], end='')
+            print('#' * self.__size)
+        return ""
